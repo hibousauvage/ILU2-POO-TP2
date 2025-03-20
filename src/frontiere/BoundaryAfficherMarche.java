50c7227835d81af015fdfbc956e06d821198405b
hibousauvage <lucas.lefebvre046@gmail.com>
2025-03-20 20:01:19 +0100

    en cours boundaryAfficherMarcher

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,19 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		if(infosMarche.length == 0) {
+			System.out.println(new StringBuilder("Le marché est vide, revenez plus tard.").toString());
+			
+		}else {
+			StringBuilder s = new StringBuilder();
+			s.append(nomAcheteur);
+			s.append(", vous touverez au marché :");
+			for(int i=0;i<infosMarche.length;i++) {
+				s.append(false);
+				//TODO
+			}
+			
+		}
 	}
 }
