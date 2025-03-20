a2a3e12fc89db6b8ce3a010eea4835d59636538c
hibousauvage <lucas.lefebvre046@gmail.com>
2025-03-20 19:08:03 +0100

    cas LibererEtal controlLibererEtal

--- src/controleur/ControlLibererEtal.java
+++ src/controleur/ControlLibererEtal.java
@@ -1,5 +1,7 @@
 package controleur;
 
+import villagegaulois.Etal;
+
 public class ControlLibererEtal {
 	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 
@@ -8,7 +10,9 @@ public class ControlLibererEtal {
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
 
-	//TODO a completer
+	public boolean isVendeur(String nomVendeur) {
+		return controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur)!=null;
+	}
 
 	/**
 	 * 
@@ -21,8 +25,12 @@ public class ControlLibererEtal {
 	 * 		[4] : quantité de produit vendu
 	 */
 	public String[] libererEtal(String nomVendeur) {
-		//TODO a completer
-		String[] donneesEtal = null;
+		
+		String[] donneesEtal = new String[4];
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		etal.libererEtal();
+		donneesEtal = etal.etatEtal();
+		
 		return donneesEtal;
 	}
 
